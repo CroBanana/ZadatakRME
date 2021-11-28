bda8e6af79b631a5116df0c90a575bc7b8e16a1e
kremazar <kmazar222@gmail.com>
2021-11-28 10:40:40 +0100

    igra

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+                anim.SetTrigger("Walk");
+            }
+
+        }
+        else
+        {
+            anim.SetTrigger("Idle");
+        }
+    }
+}
